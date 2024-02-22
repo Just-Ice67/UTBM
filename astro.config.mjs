f1a0e8c70633a5ec1240ca5a9a7910a59568002e
Just_Ice <jwice89@gmail.com>
2024-02-22 14:38:30 -0700

    Testing favicon on Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind()],
+  site: 'https://just-ice67.github.io',
+  base: 'UTBM'
 });
